9880f9c59850f0e9681a5da330ec9645d0581d7e
ZahoorOnly <i.am.zahoor.a@outlook.com>
2025-08-11 15:34:39 +0500

    fix: project initiated

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src("styles.scss").pipe(sass()).pipe(dest("css"));
+  return src("*.scss").pipe(sass()).pipe(dest("css"));
 }
 
 function watchTask() {
-  watch(["styles.scss"], buildStyles);
+  watch(["*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
